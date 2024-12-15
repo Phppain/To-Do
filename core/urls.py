81563e4d28d6d266a6f7824b7bf5f6bd899281a6
Azizali <azizali@azizali.local>
2024-12-15 22:32:58 +0500

    Third commit

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainToDo, name='mainToDo'), # Переносит на главную страницу
     path('create/', createToDo, name='createToDo'), # В зависимости от типа запроса переносит на страницу создания to-do или сохраняет to-do в БД
-    path('delete/<int:todo_id>/', deleteToDo, name='deleteToDo'), # Удаляет to-do по id
-    path('edit/<int:todo_id>/', editToDo, name='editToDo') # Позволяет изменить to-do по id
+    path(r'^delete/(?P<todo_id>\d+)/$', deleteToDo, name='deleteToDo'), # Удаляет to-do по id
+    path(r'^edit/(?P<todo_id>\d+)/$', editToDo, name='editToDo') # Позволяет изменить to-do по id
 ]
